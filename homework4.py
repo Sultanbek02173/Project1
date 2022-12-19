8e0359675f1950c5c5b0f892c75641583e7a2888
Sultanbek <sultan.02173@gmail.com>
2022-12-20 00:24:02 +0600

    homework4

---
+++ homework4.py
@@ -0,0 +1,32 @@
+class Num1:
+    def __init__(self, name) -> None:
+        self.name = name
+    
+class Num2:
+    def __init__(self, age) -> None:
+        self.age = age
+
+class Num3:
+    def prim1(self):
+        print("Hello")
+
+class Num4:
+    def prim2(self):
+        print("Goodby")
+
+class Num5(Num1, Num2, Num3, Num4):
+    def __init__(self, name, age) -> None:
+        Num1.__init__(self, name)
+        Num2.__init__(self, age)
+
+    def __str__(self) -> str:
+        return f'Name: {self.name}, age: {self.age}'
+
+
+
+
+num = Num5('Bob', 11)
+print(num)
+num.prim1()
+num.prim2()
+
